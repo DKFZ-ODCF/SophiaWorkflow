3b66e758573c357672b455c567cc2674b1bff222
Michael Heinold <m.heinold@dkfz-heidelberg.de>
2017-03-09 09:33:49 +0100

    Add insertsizesfile_list cvalue
    Add more executability tests, refined code, made more readable.

---
+++ README.SophiaWorkflow.md
@@ -0,0 +1,12 @@
+== Description
+
+
+== Run flags / switches / passable values
+
+Switch                      Default Description
+
+Value                       Description
+bamfile_list                A comma separated list of bamfiles (control, tumor ...)
+insertsizesfile_list        A list of insert sizes value files (same order as bam files!)
+
+== Changelist
--- src/de/dkfz/b080/co/sophiaworkflow/SophiaWorkflow.groovy
+++ src/de/dkfz/b080/co/sophiaworkflow/SophiaWorkflow.groovy
@@ -2,7 +2,9 @@ package de.dkfz.b080.co.sophiaworkflow
 
 import de.dkfz.b080.co.common.WorkflowUsingMergedBams
 import de.dkfz.b080.co.files.BasicBamFile
+import de.dkfz.roddy.StringConstants
 import de.dkfz.roddy.config.ConfigurationValue
+import de.dkfz.roddy.config.RecursiveOverridableMapContainerForConfigurationValues
 import de.dkfz.roddy.core.ExecutionContext
 import de.dkfz.roddy.core.ExecutionContextError
 import de.dkfz.roddy.knowledge.files.BaseFile
@@ -16,27 +18,52 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class SophiaWorkflow extends WorkflowUsingMergedBams {
 
-    private BaseFile getISizesForBam(BasicBamFile bam) {
-        return BaseFile.constructManual(LibrariesFactory.getInstance().loadRealOrSyntheticClass("InsertSizesValueFile", BaseFile.class as Class<FileObject>), new BasicBamFile(bam)) as BaseFile
+    public static final String TOOL_SOPHIA = "sophia"
+    public static final String ANALYSIS_TAG = "analysisTag"
+    public static final String ISIZES_LIST = "insertsizesfile_list"
+    public static final String BAMFILE_LIST = "bamfile_list"
+
+    /**
+     * Load (create) the class for the insert sizes file.
+     * This class actually exists in AlignmentAndQCWorkflows, but we do not want to add unnecessary dependencies.
+     * Just use Roddys synthetic class capabilities here.
+     * @return
+     */
+    Class<BaseFile> loadISizesFileClass() {
+        LibrariesFactory.getInstance().loadRealOrSyntheticClass("InsertSizesValueFile", BaseFile.class as Class<FileObject>)
+    }
+
+    /**
+     * Load the insert sizes file for a given bam file.
+     * indexInBamList is only used, if the isizes files are passed as a cvalue
+     *
+     * @return
+     */
+    BaseFile getISizesForBam(ExecutionContext context, BasicBamFile bam, int indexInBamList) {
+        def configurationValues = context.getConfigurationValues()
+        boolean isizesListIsSet = configurationValues.hasValue(ISIZES_LIST);
+        if (isizesListIsSet) {
+            List<String> isizesList = configurationValues.getString(ISIZES_LIST).split(StringConstants.SPLIT_SEMICOLON) as List<String>;
+            return BaseFile.constructSourceFile(loadISizesFileClass(), new File(isizesList[indexInBamList]), context, bam.fileStage)
+        } else {
+            return BaseFile.constructManual(loadISizesFileClass(), new BasicBamFile(bam)) as BaseFile
+        }
     }
 
     @Override
     boolean execute(ExecutionContext context, BasicBamFile bamControlMerged, BasicBamFile bamTumorMerged) {
         boolean fullWorkflow = bamControlMerged
 
-        BaseFile insertSizesValueFileForTumor = getISizesForBam(bamTumorMerged)
         if (fullWorkflow) { // Control and tumor
-            context.getConfiguration().getConfigurationValues().add(new ConfigurationValue("analysisTag", "tumorControl"))
+            context.getConfiguration().getConfigurationValues().add(new ConfigurationValue(ANALYSIS_TAG, "tumorControl"))
 
-            BaseFile insertSizesValueFileForControl = getISizesForBam(bamControlMerged)
-
-            BaseFile sophiaControlFile = call("sophia", bamControlMerged, insertSizesValueFileForControl) as BaseFile
-            BaseFile sophiaTumorFile = call("sophia", bamTumorMerged, insertSizesValueFileForTumor) as BaseFile
+            BaseFile sophiaControlFile = call(TOOL_SOPHIA, bamControlMerged, getISizesForBam(context, bamControlMerged, 0)) as BaseFile
+            BaseFile sophiaTumorFile = call(TOOL_SOPHIA, bamTumorMerged, getISizesForBam(context, bamTumorMerged, 1)) as BaseFile
 
             call("sophiaAnnotator", sophiaControlFile, sophiaTumorFile)
         } else { //NoControl!
-            context.getConfiguration().getConfigurationValues().add(new ConfigurationValue("analysisTag", "tumorOnly"))
-            BaseFile sophiaTumorFile = call("sophia", bamTumorMerged, insertSizesValueFileForTumor) as BaseFile
+            context.getConfiguration().getConfigurationValues().add(new ConfigurationValue(ANALYSIS_TAG, "tumorOnly"))
+            BaseFile sophiaTumorFile = call(TOOL_SOPHIA, bamTumorMerged, getISizesForBam(context, bamTumorMerged, 0)) as BaseFile
 
             call("sophiaAnnotatorNoControl", sophiaTumorFile)
         }
@@ -45,18 +72,27 @@ class SophiaWorkflow extends WorkflowUsingMergedBams {
     @Override
     boolean checkExecutability(ExecutionContext context) {
         boolean initialBamCheck = super.checkExecutability(context)
-        if(!initialBamCheck)
+        if (!initialBamCheck)
             return false
 
         BasicBamFile[] bamFiles = super.getInitialBamFiles(context)
+
+        def configurationValues = context.getConfigurationValues()
+        boolean isizesListIsSet = configurationValues.hasValue(ISIZES_LIST)
+        boolean bamListIsSet = configurationValues.hasValue(BAMFILE_LIST)
+        if(isizesListIsSet && ! bamListIsSet) {
+            context.addErrorEntry(ExecutionContextError.EXECUTION_NOINPUTDATA.expand("Setting ${ISIZES_LIST} is only allowed if ${BAMFILE_LIST} is set as well."))
+            return false
+        }
+
         boolean isizesfound = true;
         for (int i = 0; i < bamFiles.size(); i++) {
-            if(!bamFiles[i]) // Can be null for nocontrol
+            if (!bamFiles[i]) // Can be null for nocontrol
                 continue
 
-            def isizesFile = getISizesForBam(bamFiles[i]).path
+            def isizesFile = getISizesForBam(context, bamFiles[i], i).path
             boolean found = context.fileIsAccessible(isizesFile)
-            if(!found) {
+            if (!found) {
                 context.addErrorEntry(ExecutionContextError.EXECUTION_NOINPUTDATA.expand("The insert sizes file ${isizesFile.path} could not be found."))
                 isizesfound = false
             }
