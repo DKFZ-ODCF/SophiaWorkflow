090e4fd03fab3d751eb6c4d3b9a73fc762a7d833
Michael Heinold <m.heinold@dkfz-heidelberg.de>
2017-03-09 09:33:49 +0100

    Add insertsizesfile_list cvalue
    Summary: Add more executability tests, refined code, made more readable.
    
    Test Plan: testrun
    
    Reviewers: kensche, reisinge, ishaque
    
    Differential Revision: https://eilslabs-phabricator.dkfz.de/D351

--- src/de/dkfz/b080/co/SophiaWorkflowPlugin.java
+++ src/de/dkfz/b080/co/SophiaWorkflowPlugin.java
@@ -7,8 +7,8 @@ import de.dkfz.roddy.plugins.BasePlugin;
  */
 public class SophiaWorkflowPlugin extends BasePlugin {
 
-    public static final String CURRENT_VERSION_STRING = "1.0.11";
-    public static final String CURRENT_VERSION_BUILD_DATE = "Tue Feb 21 16:47:27 CET 2017";
+    public static final String CURRENT_VERSION_STRING = "1.0.12";
+    public static final String CURRENT_VERSION_BUILD_DATE = "Thu Mar 09 09:32:31 CET 2017";
 
     @Override
     public String getVersionInfo() {
