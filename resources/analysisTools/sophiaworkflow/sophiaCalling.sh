a8a24f5b53d1a5d870da524f7aa72a0008a02068
Michael Heinold <m.heinold@dkfz-heidelberg.de>
2017-03-24 17:38:19 +0100

    Changes according to discussion with Umut

--- resources/analysisTools/sophiaworkflow/sophiaCalling.sh
+++ resources/analysisTools/sophiaworkflow/sophiaCalling.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
+
+#IMPORTANT FILES
+#${BPS_OUT}
+#IMPORTANT FILES END
+
 source ${CONFIG_FILE}
 
-${SAMTOOLS_BINARY} view -L ${mergedRef} -F 0x600 -f 0x001 ${BAMFILE} | \
-    ${MBUFFER_BINARY} -q -m 1G -l /dev/null | \
-    ${SOPHIA_BINARY} --mergedisizes ${mergedIsizes} --clipsize ${clipThreshold} \
-                     --basequality ${qualThreshold} --basequalitylow ${qualThresholdLow} \
-                     --lowqualclipsize ${lowQualOverhangThreshold} --isizesigma ${isizeSigmaThreshold} \
-                     --bpsupport ${bpSupportThreshold} --defaultreadlength ${defaultReadLength} | \
-    ${GZIP_BINARY} --best > ${FILENAME_OUT}
+set -e
+
+${SAMTOOLS_BINARY} view -L ${mergedRef} -F 0x600 -f 0x001 ${BAMFILE} \
+  | ${MBUFFER_BINARY} -q -m 1G -l /dev/null \
+  | ${SOPHIA_BINARY} --mergedisizes ${mergedIsizes} \
+           --clipsize ${clipThreshold} \
+           --basequality ${qualThreshold} \
+           --basequalitylow ${qualThresholdLow} \
+           --lowqualclipsize ${lowQualOverhangThreshold} \
+           --isizesigma ${isizeSigmaThreshold} \
+           --bpsupport ${bpSupportThreshold} \
+           --defaultreadlength ${defaultReadLength} \
+					 | ${GZIP_BINARY} --best > ${BPS_OUT}
