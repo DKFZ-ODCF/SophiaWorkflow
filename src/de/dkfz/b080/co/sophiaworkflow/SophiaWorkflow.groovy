0b5f2be03ed133b0011175abf340a50e1e6d3faf
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2019-01-23 18:37:14 +0100

    Added submission-time check for essential configuration values.

--- src/de/dkfz/b080/co/sophiaworkflow/SophiaWorkflow.groovy
+++ src/de/dkfz/b080/co/sophiaworkflow/SophiaWorkflow.groovy
@@ -3,8 +3,11 @@ package de.dkfz.b080.co.sophiaworkflow
 import de.dkfz.b080.co.common.WorkflowUsingMergedBams
 import de.dkfz.b080.co.files.BasicBamFile
 import de.dkfz.b080.co.files.COFileStageSettings
+import de.dkfz.b080.co.files.Sample
 import de.dkfz.roddy.config.Configuration
+import de.dkfz.roddy.config.ConfigurationError
 import de.dkfz.roddy.config.ConfigurationValue
+import de.dkfz.roddy.config.RecursiveOverridableMapContainerForConfigurationValues
 import de.dkfz.roddy.core.ExecutionContext
 import de.dkfz.roddy.core.ExecutionContextError
 import de.dkfz.roddy.knowledge.files.BaseFile
@@ -26,49 +29,73 @@ class SophiaWorkflow extends WorkflowUsingMergedBams {
      * run this should be substituted by getting the data from a metadata table. Default values are not implemented.
      *
      * @param context
-     * @param sampleName        tumor, control, ...
+     * @param sampleType        tumor, control, ...
      * @return
      */
-    static LinkedHashMap<String, String> getBamParameters(Configuration config, String sampleName) {
-        // TODO: Make this workflow run with a metadata table. Probably the WorkflowUsingMergedBams has to be adapted (currently it only accepts a single pair!
-        // if (Roddy.isMetadataCLOptionSet())
-        //    SophiaMetadataTable metadataTable = new SophiaMetadataTable(MetadataTableFactory.getTable(context.analysis))
-
+    static LinkedHashMap<String, String> getBamParameters(Configuration config, Sample.SampleType sampleType) {
         LinkedHashMap<String, String> result = new LinkedHashMap()
-        ["defaultReadLength",
-         "medianIsize",
-         "stdIsizePercentage",
-         "properPairPercentage"
+        ['defaultReadLength',
+         'medianIsize',
+         'stdIsizePercentage',
+         'properPairPercentage'
         ].collectEntries(result) { varname ->
-            new MapEntry(varname, config.configurationValues.get("${sampleName}${varname.capitalize()}").toString())
+            String cvalueName = "${sampleType.name().toLowerCase()}${varname.capitalize()}"
+            String cvalue = config.configurationValues.getString(cvalueName)
+            if (cvalue.empty)
+                throw new ConfigurationError("Configuration value '${cvalueName}' is unset", config)
+            new MapEntry(varname, cvalue)
         }
-        return result
+        result
     }
 
     @Override
     boolean execute(ExecutionContext context, BasicBamFile bamControlMerged, BasicBamFile bamTumorMerged) {
         boolean fullWorkflow = bamControlMerged
 
-        context.configuration.configurationValues << new ConfigurationValue("tumorSample", (bamTumorMerged.fileStage as COFileStageSettings).sample.name)
+        Configuration configuration = context.configuration
+        RecursiveOverridableMapContainerForConfigurationValues configurationValues = configuration.configurationValues
+
+        configurationValues <<
+                new ConfigurationValue('tumorSample', (bamTumorMerged.fileStage as COFileStageSettings).sample.name)
 
         if (fullWorkflow) { // Control and tumor
-            context.configuration.configurationValues << new ConfigurationValue(ANALYSIS_TAG, "tumorControl")
-            context.configuration.configurationValues << new ConfigurationValue("controlSample", (bamControlMerged.fileStage as COFileStageSettings).sample.name)
+            configurationValues <<
+                    new ConfigurationValue(ANALYSIS_TAG, 'tumorControl')
+            configurationValues <<
+                    new ConfigurationValue('controlSample', (bamControlMerged.fileStage as COFileStageSettings).sample.name)
 
             BaseFile sophiaControlFile =
-                    run(TOOL_SOPHIA, bamControlMerged, getBamParameters(context.configuration, "control")) as BaseFile
+                    run(TOOL_SOPHIA, bamControlMerged,
+                            getBamParameters(configuration, bamControlMerged.sample.sampleType)) as BaseFile
             BaseFile sophiaTumorFile =
-                    run(TOOL_SOPHIA, bamTumorMerged, getBamParameters(context.configuration, "tumor")) as BaseFile
+                    run(TOOL_SOPHIA, bamTumorMerged,
+                            getBamParameters(configuration, bamTumorMerged.sample.sampleType)) as BaseFile
 
-            run("sophiaAnnotator", sophiaControlFile, sophiaTumorFile)
-        } else { //NoControl!
-            context.getConfiguration().getConfigurationValues().add(new ConfigurationValue(ANALYSIS_TAG, "tumorOnly"))
+            run('sophiaAnnotator', sophiaControlFile, sophiaTumorFile)
+        } else { // NoControl!
+            configurationValues <<
+                    new ConfigurationValue(ANALYSIS_TAG, 'tumorOnly')
             BaseFile sophiaTumorFile =
-                    run(TOOL_SOPHIA, bamTumorMerged, getBamParameters(context.configuration, "tumor")) as BaseFile
+                    run(TOOL_SOPHIA, bamTumorMerged,
+                            getBamParameters(configuration, bamTumorMerged.sample.sampleType)) as BaseFile
 
-            run("sophiaAnnotatorNoControl", sophiaTumorFile)
+            run('sophiaAnnotatorNoControl', sophiaTumorFile)
         }
     }
+
+    /* Move the following three functions to a superclass. */
+    List<BasicBamFile> getControlBamFiles() {
+        loadInitialBamFilesForDataset(context).findAll { it.sample.sampleType == Sample.SampleType.CONTROL } as List
+    }
+
+    List<BasicBamFile> getTumorBamFiles() {
+        loadInitialBamFilesForDataset(context).findAll { it.sample.sampleType == Sample.SampleType.TUMOR } as List
+    }
+
+    List<BasicBamFile> getUnkownBamFiles() {
+        loadInitialBamFilesForDataset(context).findAll { it.sample.sampleType == Sample.SampleType.UNKNOWN } as List
+    }
+
 
     @Override
     boolean checkExecutability(ExecutionContext context) {
@@ -81,8 +108,21 @@ class SophiaWorkflow extends WorkflowUsingMergedBams {
         def configurationValues = context.getConfigurationValues()
         boolean bamListIsSet = configurationValues.hasValue(BAMFILE_LIST)
         if(! bamListIsSet) {
-            context.addErrorEntry(ExecutionContextError.EXECUTION_NOINPUTDATA.expand("${BAMFILE_LIST} is not set."))
+            context.addError(ExecutionContextError.EXECUTION_NOINPUTDATA.expand("${BAMFILE_LIST} is not set."))
             return false
+        }
+
+        try {
+            getBamParameters(context.configuration, Sample.SampleType.TUMOR)
+        } catch (ConfigurationError e) {
+            context.addError(ExecutionContextError.EXECUTION_SETUP_INVALID.expand(e.message))
+        }
+        if (!controlBamFiles.empty) {
+            try {
+                getBamParameters(context.configuration, Sample.SampleType.CONTROL)
+            } catch (ConfigurationError e) {
+                context.addError(ExecutionContextError.EXECUTION_SETUP_INVALID.expand(e.message))
+            }
         }
 
         return true
