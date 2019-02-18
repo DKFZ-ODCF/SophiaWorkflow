eee7de28b0bc73d3a85f0fb16ac0b14a6496df98
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2019-02-18 18:25:30 +0100

    Compile for version number reporting.

--- src/de/dkfz/b080/co/SophiaWorkflowPlugin.java
+++ src/de/dkfz/b080/co/SophiaWorkflowPlugin.java
@@ -7,8 +7,8 @@ import de.dkfz.roddy.plugins.BasePlugin;
  */
 public class SophiaWorkflowPlugin extends BasePlugin {
 
-    public static final String CURRENT_VERSION_STRING = "2.1.0";
-    public static final String CURRENT_VERSION_BUILD_DATE = "Fri Dec 07 12:44:04 CET 2018";
+    public static final String CURRENT_VERSION_STRING = "2.1.2";
+    public static final String CURRENT_VERSION_BUILD_DATE = "Mon Feb 18 18:30:05 CET 2019";
 
     @Override
     public String getVersionInfo() {
