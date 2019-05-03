1f19f09897471d2c47adc62824537c5656f16595
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2019-05-03 10:53:52 +0200

    Autoreformat all Python code that lacked proper usage of spaces.

--- resources/analysisTools/sophiaworkflow/RNAdecontaminationStep1.py
+++ resources/analysisTools/sophiaworkflow/RNAdecontaminationStep1.py
@@ -16,24 +16,25 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-from sys import argv,stderr
+from sys import argv, stderr
 from itertools import product
-preFilteredBedpe=argv[1]
 
-broadProd=list(product(range(51),range(51)))
-narrowProd=list(product(range(10),range(10)))
+preFilteredBedpe = argv[1]
 
-lineIndex=0
+broadProd = list(product(range(51), range(51)))
+narrowProd = list(product(range(10), range(10)))
+
+lineIndex = 0
 with open(preFilteredBedpe) as inputHandle:
     for line in inputHandle:
-        if line[0]!='#':
-            lineChunks=line.rstrip().split('\t')
-            eventType=lineChunks[8]
-            if lineChunks[0]==lineChunks[3] and eventType in {"TRA","DEL"}:
-                minPos=min(int(lineChunks[1]),int(lineChunks[4]))
-                maxPos=max(int(lineChunks[1]),int(lineChunks[4]))
-                eventScore=int(lineChunks[9])
+        if line[0] != '#':
+            lineChunks = line.rstrip().split('\t')
+            eventType = lineChunks[8]
+            if lineChunks[0] == lineChunks[3] and eventType in {"TRA", "DEL"}:
+                minPos = min(int(lineChunks[1]), int(lineChunks[4]))
+                maxPos = max(int(lineChunks[1]), int(lineChunks[4]))
+                eventScore = int(lineChunks[9])
                 for iterPair in broadProd:
-                    if (minPos-iterPair[0]) < maxPos-1+iterPair[1]:
-                        print(lineChunks[0],minPos-iterPair[0],maxPos-1+iterPair[1],lineIndex,sep='\t')
-            lineIndex+=1
+                    if (minPos - iterPair[0]) < maxPos - 1 + iterPair[1]:
+                        print(lineChunks[0], minPos - iterPair[0], maxPos - 1 + iterPair[1], lineIndex, sep='\t')
+            lineIndex += 1
--- resources/analysisTools/sophiaworkflow/getIntrachromosomalRanges.py
+++ resources/analysisTools/sophiaworkflow/getIntrachromosomalRanges.py
@@ -17,12 +17,13 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 from sys import argv
-inputBedpe=argv[1]
+
+inputBedpe = argv[1]
 with open(inputBedpe) as f:
-    for i,line in enumerate(f):
-        lineChunks=line.rstrip().split('\t')
-        if lineChunks[0]!=lineChunks[3]:
-            print(lineChunks[0],int(9e9),int(9e9+1),i,sep='\t')
+    for i, line in enumerate(f):
+        lineChunks = line.rstrip().split('\t')
+        if lineChunks[0] != lineChunks[3]:
+            print(lineChunks[0], int(9e9), int(9e9 + 1), i, sep='\t')
         else:
-            minPos,maxPos=sorted([int(lineChunks[1]),int(lineChunks[4])])
-            print(lineChunks[0],minPos,maxPos,i,sep='\t')
+            minPos, maxPos = sorted([int(lineChunks[1]), int(lineChunks[4])])
+            print(lineChunks[0], minPos, maxPos, i, sep='\t')
