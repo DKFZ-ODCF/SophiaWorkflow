2abc3d396e3ec0042b50e490ec5db15afa2aad29
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2020-08-28 12:37:50 +0200

    Refactered and added a bit of documentation to intersectionCollapsing.py.

--- resources/analysisTools/sophiaworkflow/intersectionCollapsing.py
+++ resources/analysisTools/sophiaworkflow/intersectionCollapsing.py
@@ -15,56 +15,73 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
+#
+#
+# Aggregate all annotations for rows with the same ID column value (1-based: column 4).
+# Groups containing no information (no genes, cancer genes, or super-enhancers) are
+# *not* printed.
+#
+# Usage:
+#         intersectionCollapsing.py ${name}directHits[12]Pre
+#
+# The input file comes from bedtools intersect. See sophiaAnnotateAbridgedCaller.sh for details..
+#
 from sys import argv
 
-inputFile = argv[1]
+
+def set_join(the_set: set, sep=",", default="."):
+    if len(the_set) != 0:
+        return sep.join(the_set)
+    else:
+        return default
 
 
-def print_line(hit_str: str, cancer_hit_str: str, super_enhancer_hit_str: str):
+def print_line(gene_hit_string: str, cancer_hit_str: str, super_enhancer_hit_str: str):
     """Print a line of information unless it is trivial/all '.'"""
-    if hit_str != "." or cancer_hit_str != "." or super_enhancer_hit_str != ".":
-        print(hit_str, cancer_hit_str, super_enhancer_hit_str, sep='\t')
+    if gene_hit_string != "." or cancer_hit_str != "." or super_enhancer_hit_str != ".":
+        print(gene_hit_string, cancer_hit_str, super_enhancer_hit_str, sep='\t')
+
+
+def print_hits(gene_hits: set, cancer_gene_hits: set, super_enhancer_hits: set):
+    print_line(set_join(gene_hits),
+               set_join(cancer_gene_hits),
+               set_join(super_enhancer_hits))
+
+
+inputFile = argv[1]
 
 
 lastID = ""
 geneHits = set()
 cancerGeneHits = set()
 superEnhancerHits = set()
+
 with open(inputFile) as f:
     for line in f:
         lineChunks = line.rstrip().split('\t')
         ID = lineChunks[3]
+
         if ID != lastID:
+
             if lastID != "":
-                hitStr = "."
-                cancerHitStr = "."
-                superEnhancerHitStr = "."
-                if len(geneHits) != 0:
-                    hitStr = ','.join(geneHits)
-                if len(cancerGeneHits) != 0:
-                    cancerHitStr = ','.join(cancerGeneHits)
-                if len(superEnhancerHits) != 0:
-                    superEnhancerHitStr = ','.join(superEnhancerHits)
-                print_line(hitStr, cancerHitStr, superEnhancerHitStr)
+                print_hits(geneHits, cancerGeneHits, superEnhancerHits)
+
             lastID = ID
             geneHits = set()
             cancerGeneHits = set()
             superEnhancerHits = set()
-        if lineChunks[4] != ".":
-            if lineChunks[4] == "1":
-                geneHits.add(lineChunks[8])
-            elif lineChunks[4] == "2":
-                cancerGeneHits.add(lineChunks[8])
-            elif lineChunks[4] == "3":
-                superEnhancerHits.add(lineChunks[8])
-    hitStr = "."
-    cancerHitStr = "."
-    superEnhancerHitStr = "."
-    if len(geneHits) != 0:
-        hitStr = ','.join(geneHits)
-    if len(cancerGeneHits) != 0:
-        cancerHitStr = ','.join(cancerGeneHits)
-    if len(superEnhancerHits) != 0:
-        superEnhancerHitStr = ','.join(superEnhancerHits)
-    print_line(hitStr, cancerHitStr, superEnhancerHitStr)
+
+        hitClass = lineChunks[4]
+        if hitClass != ".":
+            annotation = lineChunks[8]
+            if hitClass == "1":
+                geneHits.add(annotation)
+            elif hitClass == "2":
+                cancerGeneHits.add(annotation)
+            elif hitClass == "3":
+                superEnhancerHits.add(annotation)
+            else:
+                raise "Unknown hit class (.=unclassified, 1=gene, 2=cancerGene, 3=superEnhancer): '%'".\
+                    format(hitClass)
+
+    print_hits(geneHits, cancerGeneHits, superEnhancerHits)
